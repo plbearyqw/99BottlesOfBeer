a57adc6aa155f5e9131f131ba2f6ce741bb68be9
plbearyqw <yjliengine@163.com>
2017-08-10 12:33:08 +0000

    99 bottles of beer

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -6,25 +6,51 @@ var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
 
-var main = require("../lib/main.js");
+var m = require("../lib/main.js");
+var main = m.main;
+var printByNumbers = m.printByNumbers;
+var printLastPart = m.printLastPart;
 
 
 describe("测试描述", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
+    it("测试printByNumbers(3-99)", function(){
 
-        var result = main();
-        var expect_string = '';
+        var inputs = 99;
+        var result = printByNumbers(inputs);
+        var expect_string = '99 bottles of beer on the wall, 99 bottles of beer.\n' +
+                              'Take one down and pass it around, 98 bottles of beer on the wall.\n';
+        
+        expect(expect_string).to.equal(result);
+    });
+    
+    it("测试printByNumbers(2)", function(){
+
+        var inputs = 2;
+        var result = printByNumbers(inputs);
+        var expect_string = '2 bottles of beer on the wall, 2 bottles of beer.\n' +
+                              'Take one down and pass it around, 1 bottle of beer on the wall.\n';
+        
+        expect(expect_string).to.equal(result);
+    });
+    
+    it("测试printByNumbers(1)", function(){
+
+        var inputs = 1;
+        var result = printByNumbers(inputs);
+        var expect_string = '1 bottle of beer on the wall, 1 bottle of beer.\n' +
+                              'Take one down and pass it around, no more bottles of beer on the wall.\n';
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
+    it("测试printLastPart", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+        
+        var result = printLastPart();
+        var expect_string = 'No more bottles of beer on the wall, no more bottles of beer.\n' +
+                              'Go to the store and buy some more, 99 bottles of beer on the wall.';
 
         expect(expect_string).to.equal(result);
     });
